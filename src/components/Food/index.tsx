a62ce8d195ef1e78358d7a1b20831c0499dbcb4b
Wesley Feitosa <jwesleydasilva@gmail.com>
2020-06-02 09:53:07 -0300

    available food

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
+import api from '../../services/api';
 
 interface IFoodPlate {
   id: number;
@@ -28,6 +29,8 @@ const Food: React.FC<IProps> = ({
 
   async function toggleAvailable(): Promise<void> {
     setIsAvailable(!isAvailable);
+
+    await api.put(`/foods/${food.id}`, { ...food, available: !isAvailable });
   }
 
   function setEditingFood(): void {
